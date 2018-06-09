b11f8818bb11e024de359215b42c2cd507f6f829
thomask333 <thomasking.gs@outlook.com>
2018-06-09 15:49:16 -0400

    advanced character editing(remembers previous stats) and character check(checks if user has an existing character in game)

--- app/assets/javascripts/newchar.js
+++ app/assets/javascripts/newchar.js
@@ -1,7 +1,17 @@
- 
-var skillpoints = 7;
-document.getElementById('points').innerHTML= skillpoints;
+ var skillpoints;
 
+document.getElementById('points').innerHTML= skillpoints;
+if(document.getElementById('character_brawn').value == 0){
+	document.getElementById('character_brawn').value = 1;
+	document.getElementById('brawn').innerHTML = document.getElementById('character_brawn').value;
+	skillpoints = 7;
+	document.getElementById('points').innerHTML= skillpoints;
+}
+else{
+document.getElementById('brawn').innerHTML = document.getElementById('character_brawn').value;
+var skillpoints = 0;
+document.getElementById('points').innerHTML= skillpoints;
+}
 
 
 function addPoint(test, test1){
@@ -12,9 +22,10 @@ document.getElementById('points').innerHTML= skillpoints;
 if(skillpoints == 0){
 	var subButtion = document.getElementsByClassName("btn btn-info");
 	subButtion[1].style.display = 'initial';
+	document.getElementById('char_submit').style.display ="initial";
 		}
 	}
-}
+};
 
 // else{
 // 	
@@ -24,13 +35,14 @@ if(skillpoints <= 6 && test.value > 1){
 test.value = parseInt(test.value) - 1;
 skillpoints = skillpoints + 1;
 test1.innerHTML = test.value;
-document.getElementById('points').innerHTML= skillpoints;}
+document.getElementById('points').innerHTML= skillpoints;
 if(skillpoints != 0){
 	var subButtion = document.getElementsByClassName("btn btn-info");
 	subButtion[1].style.display = 'none';
 		}
-
+	}
 }
+
 function hideMe(){
 	var hide = document.getElementsByClassName("row");
 	var charButtons = document.getElementsByClassName("btn btn-info");
@@ -45,7 +57,6 @@ function hideMe(){
     document.getElementById("practices").innerHTML = "Practices: " + parseInt(practice);
     hide[2].style.display = "initial";
 ;
-
 }
 function showMe(){
 	var charButtons = document.getElementsByClassName("btn btn-info");
@@ -59,3 +70,47 @@ function showMe(){
 	charButtons[1].style.display = "block";
 	hide[2].style.display = "none";
 }
+
+function setValues(){
+	if(
+		document.getElementById('character_brawn').value == 0 ||
+		document.getElementById('character_wit').value == 0 ||
+		document.getElementById('character_charm').value == 0 ||
+		document.getElementById('character_intelligence').value == 0 ||
+		document.getElementById('character_agility').value == 0 ||
+		document.getElementById('character_presense').value == 0 
+		){
+	document.getElementById('character_brawn').value = 1;
+	document.getElementById('character_wit').value =1;
+	document.getElementById('character_charm').value =1;
+	document.getElementById('character_intelligence').value =1;
+	document.getElementById('character_agility').value =1;
+	document.getElementById('character_presense').value =1;
+
+	document.getElementById('brawn').innerHTML = document.getElementById('character_brawn').value;
+	document.getElementById('agility').innerHTML = document.getElementById('character_agility').value;
+	document.getElementById('wit').innerHTML = document.getElementById('character_wit').value;
+	document.getElementById('charm').innerHTML = document.getElementById('character_charm').value;
+	document.getElementById('intelligence').innerHTML = document.getElementById('character_intelligence').value;
+	document.getElementById('presense').innerHTML = document.getElementById('character_presense').value;
+	skillpoints = 7;
+	document.getElementById('points').innerHTML= skillpoints;
+			}
+else{
+document.getElementById('brawn').innerHTML = document.getElementById('character_brawn').value;
+document.getElementById('agility').innerHTML = document.getElementById('character_agility').value;
+document.getElementById('wit').innerHTML = document.getElementById('character_wit').value;
+document.getElementById('charm').innerHTML = document.getElementById('character_charm').value;
+document.getElementById('intelligence').innerHTML = document.getElementById('character_intelligence').value;
+document.getElementById('presense').innerHTML = document.getElementById('character_presense').value;
+var skillpoints = 0;
+document.getElementById('points').innerHTML= skillpoints;
+if(skillpoints == 0){
+	var subButtion = document.getElementsByClassName("btn btn-info");
+	subButtion[1].style.display = 'initial';
+	document.getElementById('char_submit').style.display ="initial";
+		}
+		}
+}
+
+setValues();
